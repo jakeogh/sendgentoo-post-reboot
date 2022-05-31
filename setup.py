6e85177e19f4fda9bfd26bb88a8422e73ba7dafa
jakeogh <github.com@v6y.net>
2022-05-30 17:47:55 -0700

    auto-commit

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
 import fastentrypoints
-from setuptools import find_packages, setup
+
 if not sys.version_info[0] == 3:
-    sys.exit("Python 3 is required. Use: \'python3 setup.py install\'")
+    sys.exit("Python 3 is required. Use: 'python3 setup.py install'")
 
 dependencies = ["icecream", "click"]
 
@@ -17,8 +21,8 @@ config = {
     "author_email": "github.com@v6y.net",
     "description": "takes a minimal sendgentoo install to my setup",
     "long_description": __doc__,
-    "packages": find_packages(exclude=['tests']),
-    "package_data": {"sendgentoo_post_reboot": ['py.typed']},
+    "packages": find_packages(exclude=["tests"]),
+    "package_data": {"sendgentoo_post_reboot": ["py.typed"]},
     "include_package_data": True,
     "zip_safe": False,
     "platforms": "any",
